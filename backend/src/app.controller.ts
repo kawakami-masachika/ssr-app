44fe859f3c26b69fe0f40af3fa61afd37f498d72
kazuki765 <kazuki.kujira@gmail.com>
2023-01-09 20:36:55 +0900

    環境構築用のmakefile作成

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,8 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): { hello: string } {
-    return { hello: this.appService.getHello()
-    };
+  getHello(): string {
+    return this.appService.getHello();
   }
 }

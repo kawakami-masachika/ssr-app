41e15118ebb82e4f47f987fcecf91ac7504a9281
kawakami-masachika <supercell1023koeda@gmail.com>
2023-01-09 13:26:00 +0900

    ftont backend疎通

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home(props: {hello: string}) {
   return (
     <>
       <Head>
         <title>ssr app</title>
       </Head>
+      <div>
+        <p>{props.hello}</p>
+      </div>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3200/');
+  const json = await res.json();
+
+  return {props: json}
 }

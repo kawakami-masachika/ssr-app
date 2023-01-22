f313e6f74e08de4b4957c8cd4f2c2999d4280d71
kawakami-masachika <supercell1023koeda@gmail.com>
2023-01-22 12:08:37 +0900

    登録フォームの自走

--- frontend/pages/payments/new.tsx
+++ frontend/pages/payments/new.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { useForm } from "react-hook-form";
 
 export type CategoryProps = {
   data: {
@@ -11,12 +12,48 @@ export type CategoryProps = {
 }
 
 const AddPayments = ({ data }: CategoryProps) => {
-  const payments = data.items.map((p) => <li key={p.id}>{p.name}</li>)
+  const { register, handleSubmit, formState: { errors }} = useForm();
+  const categories = data.items.map((c) => <option key={c.id} value={c.code }>{c.name}</option>)
+  const onSubmit = (data: any) => console.log(data);
   return (
     <>
-      <div>支払い登録</div>
-      <ul>{payments}</ul>
-      <Link href="/">HOME</Link>
+      <div className="container">
+        <h3>支払い登録</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="paymentName">支払い内容</label>
+            <input {...register('name', { required: true, maxLength: 20 })} type="text" id="paymentName" />
+            {errors.name?.type === 'required' && <p role="alert">支払い内容は必須です</p>}
+          </div>
+
+          <div>
+            <label htmlFor="paymentPrice">金額</label>
+            <input {...register('price', { required: true, min: 1, max: 100000000 })} type="number" id="paymentPrice" />
+            {errors.price?.type === 'required' && <p role="alert">金額は必須です</p>}
+          </div>
+
+          <div>
+            <label htmlFor="paymentMemo">メモ</label>
+            <textarea
+              {...register('memo', { required: false, maxLength: 300 })}
+              name="memo"
+              id="paymentMemo"
+            ></textarea>
+          </div>
+
+          <div>
+            <label htmlFor="category">支払いカテゴリー</label>
+            <select {...register('category', { required: true })} id="category">
+              {categories}
+            </select>
+            {errors.category?.type === 'required' && <p role="alert">支払いカテゴリーを選択してください</p>}
+          </div>
+
+          <input type="submit" value="登録" />
+        </form>
+
+        <Link href="/">HOME</Link>
+      </div>
     </>
   )
 }

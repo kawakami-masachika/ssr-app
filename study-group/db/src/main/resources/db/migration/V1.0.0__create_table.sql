6b721010e9de2b5a3781ada16338652d572e606b
kazuki765 <kazuki.kujira@gmail.com>
2023-07-01 23:41:09 +0900

    カラム名変更

--- study-group/db/src/main/resources/db/migration/V1.0.0__create_table.sql
+++ study-group/db/src/main/resources/db/migration/V1.0.0__create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE if not exists "user" (
+CREATE TABLE if not exists users (
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     handle_name VARCHAR(255) NOT NULL,
--- study-group/db/src/main/resources/db/migration/V1.0.1__create_table.sql
+++ study-group/db/src/main/resources/db/migration/V1.0.1__create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE if not exists "group" (
+CREATE TABLE if not exists groups (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INT NOT NULL,
   name VARCHAR NOT NULL,
@@ -6,15 +6,15 @@ CREATE TABLE if not exists "group" (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL,
   deleted_at TIMESTAMP DEFAULT NULL,
-  FOREIGN KEY (owner_id) REFERENCES "user" (id)
+  FOREIGN KEY (owner_id) REFERENCES users (id)
 );
 
-CREATE TABLE if not exists user_group (
+CREATE TABLE if not exists users_groups (
   user_id INT NOT NULL,
   group_id INT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL,
   deleted_at TIMESTAMP DEFAULT NULL,
-  FOREIGN KEY (user_id) REFERENCES "user" (id),
-  FOREIGN KEY (group_id) REFERENCES "group" (id)
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (group_id) REFERENCES groups (id)
 );

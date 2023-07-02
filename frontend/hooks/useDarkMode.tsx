dfa588f767a2d0e2da6af5f71530a0680e8f2457
masachikakawakami <masachika.kawakami@cct-inc.co.jp>
2023-07-02 18:26:32 +0900

    frontend first commit

--- frontend/hooks/useDarkMode.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { ThemeContext } from '../context/ThemeProvider'
-export const useDarkMode = () => {
-  const { darkMode, setDarkMode } = useContext(ThemeContext)
-  const className = darkMode ? 'dark-scheme' : 'light-scheme'
-  const toggleDarkMode = () => setDarkMode && setDarkMode(!darkMode)
-  return { className: className, toggleDarkMode }
-}

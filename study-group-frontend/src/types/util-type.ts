43ca4ed408dabcaa7a6c17d6797c6243d9de875e
kawakami-masachika <supercell1023koeda@gmail.com>
2023-07-16 10:41:33 +0900

    add utility type

--- study-group-frontend/src/types/util-type.ts
+++ study-group-frontend/src/types/util-type.ts
@@ -1,19 +1,19 @@
 export type OptionalProps<T, K extends keyof T> = {
-  [P in Exclude<keyof T, K>] : T[P]
+  [P in Exclude<keyof T, K>]: T[P];
 } & {
-  [Q in K]? : T[Q]
-}
+  [Q in K]?: T[Q];
+};
 
 export type RequiredProps<T, K extends keyof T> = {
-  [P in Exclude<keyof T, K>] : T[P]
+  [P in Exclude<keyof T, K>]: T[P];
 } & {
-  [Q in K]-?: T[Q]
-}
+  [Q in K]-?: T[Q];
+};
 
 export type ReadOnlyProps<T, K extends keyof T> = {
-  [P in Exclude<keyof T, K>] : T[P]
+  [P in Exclude<keyof T, K>]: T[P];
 } & {
-  readonly [Q in K]: T[Q]
-}
+  readonly [Q in K]: T[Q];
+};
 
-export type Valueof<T> = T[keyof T]
+export type Valueof<T> = T[keyof T];

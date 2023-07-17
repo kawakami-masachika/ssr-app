96fe585fb413e91e67f076d8c6bfd9f38a4e6488
kawakami-masachika <supercell1023koeda@gmail.com>
2023-07-18 08:50:54 +0900

    button componentの修正

--- study-group-frontend/src/app/components/Buttons/ButtonBasic.tsx
+++ study-group-frontend/src/app/components/Buttons/ButtonBasic.tsx
@@ -1,34 +1,43 @@
-'use client';
+import React, { ReactNode } from 'react';
 
-import React from 'react';
-import { ButtonProps } from '@/types/components/button';
+export type Props = React.ComponentPropsWithoutRef<"button"> & {
+  buttonText: string;
+  variant?: 'contained' | 'outline';
+  fullWidth?: boolean;
+  size?: 'xs' | 'sm' | 'medium' | 'large';
+  color?: 'info' | 'warning' | 'error';
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+}
 
 export const ButtonBasic = ({
   disabled = false,
   color = 'info',
   variant = 'contained',
   size = 'medium',
+  type = 'submit',
   ...props
-}: ButtonProps) => {
-  const [buttonStyles, setButtonStyles] = React.useState<string[]>([
-    'btn',
-    `btn-${color}`,
-  ]);
-  React.useCallback(() => {
-    if (variant === 'outline') buttonStyles.push('btn-outline');
-    if (size !== 'medium') buttonStyles.push(`btn-${size}`);
-    setButtonStyles(buttonStyles);
-  }, [buttonStyles, variant, size]);
+}: Props) => {
+
+  const buttonStyles = () => {
+    const baseStyles = ['btn', `btn-${color}`]
+    if(variant === 'outline') baseStyles.push('btn-outline');
+    if(size !== 'medium') baseStyles.push(`btn-${size}`);
+    if(props.fullWidth) baseStyles.push(`btn-block`)
+    return baseStyles
+  };
 
   return (
     <>
       <button
         disabled={disabled}
-        className={buttonStyles.join(' ')}
-        onClick={props.onClickButton}
+        className={buttonStyles().join(' ')}
+        onClick={props.onClick}
+        type={type}
       >
         {props.startIcon ? props.startIcon : null}
         {props.buttonText}
+        {props.startIcon ? props.endIcon : null}
       </button>
     </>
   );
